c1b6363adf49982de3c0a3b00ce4efaf3777a75d
aagam109 <aagamshah1090@gmail.com>
2019-09-29 12:27:55 +0530

    Updated

--- app.js
+++ app.js
@@ -33,6 +33,19 @@ app.post("/",function(req,res)
 {
   var email = req.body.email;
   var password = req.body.pass;
+  var sql = "Select Email,Password from user where Email = " + mysql.escape(email) + " and Password = " + mysql.escape(password);
+  con.query(sql,function(err,result)
+  {
+    if(err)
+    {
+      app.redirect('/');
+      console.log('Incorrect Id and Password');
+    }
+    else
+    {
+      res.redirect('/index.html');
+    }
+  });
 });
 
 //Register Page Redering 
@@ -43,10 +56,31 @@ app.get('/register',function(req,res)
 
 app.post('/register',function(req,res)
 {
-
-
+  var name = req.body.name;
+  var email = req.body.email;
+  var pass = req.body.pass;
+  var phoneno = req.body.phoneno;
+  var sql = "Insert into user values ( " + mysql.escape(name) + ","  + mysql.escape(email) + "," + mysql.escape(pass) + " , "  + mysql.escape(phoneno) + " )";
+  con.query(sql,function(err,result)
+  {
+    if(err)
+    {
+      res.redirect('/register');
+    }
+    else
+    {
+      console.log('Value Inserted');
+      res.redirect('/index');
+    }
+    
+  })
 })
 
+//Index Page Rendering
+app.get('/index.html',function(req,res)
+{
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // Port Declaration of the Server
 app.listen(3000,function()
